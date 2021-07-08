059439611d9b7b74c005dba9501d3f63c26f6c98
github username <github email address>
2021-07-07 20:30:53 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Switch } from 'react-router-dom';
+import Home from './Home'
+import { useHistory } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const history = useHistory();
+  
+
+
+return (
+ <Router>
+<div class="flex-grid-center">
+  <div class="pure-button arcade-button blue">
+	 BEGIN
+  </div>
+	
+</div>
+ <div className="app">
+
+ 
+    <Route exact path='/'>
+
+      <Home/>
+    </Route>
+    
     </div>
+ </Router>
   );
 }
 
